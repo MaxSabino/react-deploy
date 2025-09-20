2041ac2ef516fb0c563bc1b935860414f2385bbd
Max Sabino <sabinobkp2022@gmail.com>
2025-09-19 22:43:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hellow Word!!!</h1>
+      <h2>Netlify Teste Deploy</h2>
+      <div>
+        <h3>{count}</h3>
+        <button onClick={() => setCount((prev) => prev + 1)}>INCREMENT</button>
+        <img src={logo} />
+      </div>
+    </>
   );
+
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>
+  //       Edit <code>src/App.js</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React
+  //     </a>
+  //   </header>
+  // </div>
 }
 
 export default App;
